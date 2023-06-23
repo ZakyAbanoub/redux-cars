381e6391d3ddb57d3e4994a7d6b0ec548ce96057
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-06-24 00:46:20 +0300

    delete actions file

--- src/store/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-const CREATE_AND_RESET = "create_and_reset";
-
-export const createAndReset = createAction(CREATE_AND_RESET, (name, cost) => ({
-  name,
-  cost,
-}));
